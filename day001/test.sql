47e83133f72eb6b9d6ea3bef31b6bcf837003713
minch13r <0414minyoung@naver.com>
2025-02-13 09:36:04 +0900

    졸업생 테이블 생성, 기존 테이블 구조 복사

--- day001/test.sql
+++ day001/test.sql
@@ -1,6 +1,3 @@
 use test;
 
-update student set grade = 1 where age = 20;
-update student set grade = 2 where age = 21;
-update student set grade = 3 where age = 22;
-update student set grade = 4 where age >= 23;
+create table if not exists student_4 like student;
