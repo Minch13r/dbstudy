b14f05c7818edbfc499238e43d834c304f310776
minch13r <0414minyoung@naver.com>
2025-02-13 11:30:10 +0900

    buy 테이블에 product 테이블에서 select 한 것들 insert

--- day001/day03.sql
+++ day001/day03.sql
@@ -1,4 +1,25 @@
 use shop;
 
 insert into buy(customer, product_name, price, amount)
-(select '홍길동', name, price, 5 from product where num = 1)
+(select '이순신', name, price, 2 from product where num = 4);
+
+insert into buy(customer, product_name, price, amount)
+(select '을지문덕', name, price, 7 from product where num = 6);
+
+insert into buy(customer, product_name, price, amount)
+(select '이성계', name, price, 3 from product where num = 7);
+
+insert into buy(customer, product_name, price, amount)
+(select '주몽', name, price, 4 from product where num = 10);
+
+insert into buy(customer, product_name, price, amount)
+(select '대조영', name, price, 1 from product where num = 8);
+
+insert into buy(customer, product_name, price, amount)
+(select '선덕여왕', name, price, 2 from product where num = 9);
+
+insert into buy(customer, product_name, price, amount)
+(select '박혁거세', name, price, 10 from product where num = 3);
+
+insert into buy(customer, product_name, price, amount)
+(select '김구', name, price, 10 from product where num = 2);
