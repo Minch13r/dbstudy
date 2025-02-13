6e5443d7006d6a4186e462ccd31ecd645191bd43
minch13r <0414minyoung@naver.com>
2025-02-13 17:19:09 +0900

    transaction을 사용하여 9월에 입고된 재고 상품을 product_non로 이동 => 미리    product_non 테이블은 product와 같은 형식으로 생성    기존 product 테이블에서는 삭제    완료되면 commit;

--- day001/day03.sql
+++ day001/day03.sql
@@ -1,3 +1,16 @@
 use shop;
 
-select month(register_date) as 월별, sum(price * sale_amount) as 월별매출 from product group by 월별 order by 월별 desc;
+  -- 트랜잭션 시작
+start transaction;
+
+-- product와 같은 형식으로 product_non 테이블 생성
+create table if not exists product_non like product;
+
+-- 9월에 입고된 재고 상품을 product_non으로 이동
+insert into product_non (select * from product where month(product.register_date) = 9);
+
+-- 기존 product 테이블에서는 삭제
+delete from product where month(register_date) = 9;
+
+-- 완료되면 commit
+commit;
