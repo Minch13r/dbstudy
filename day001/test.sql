a5fc6de09eda69d6423cac4f03927687e7beeddb
minch13r <0414minyoung@naver.com>
2025-02-13 09:32:23 +0900

    데이터 수정 1번 유형

--- day001/test.sql
+++ day001/test.sql
@@ -1,7 +1,3 @@
 use test;
 
-insert into test2 (name, age, address) values ('홍길동', 23, '서울');
-insert into test2 (name, age, address) values ('강길순', 24, '인천');
-insert into test2 (name, age, address) values ('이순신', 22, '서울');
-insert into test2 (name, age, address) values ('강감찬', 23, '인천');
-insert into test2 (name, age, address) values ('유관순', 21, '서울');
+update test2 set age = 20 where id = 4 or id = 5;
