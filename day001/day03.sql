c986671b7dd169109e53f68d8259963ff6753138
minch13r <0414minyoung@naver.com>
2025-02-13 16:51:53 +0900

    price가 가장 높은(가장 비싼상품) 출력

--- day001/day03.sql
+++ day001/day03.sql
@@ -1,5 +1,3 @@
 use shop;
 
-
-
-select num, CONCAT(LEFT(customer, 1), 'O', RIGHT(customer, 1)) as customer, product_name, price, amount, total, buy_date FROM buy
+select * from product order by price asc limit 9,10;
