7b612b7f445f04d29ff73f8edff0ea6a2c93f491
minch13r <0414minyoung@naver.com>
2025-02-13 16:52:25 +0900

    product 테이블에서 price가 40000이상인 데이터만 상품명(할인상품)으로 표시 => select

--- day001/day03.sql
+++ day001/day03.sql
@@ -1,3 +1,6 @@
 use shop;
 
-select * from product order by price asc limit 9,10;
+select num, type_a, type_b,
+       case when price >= 40000 then concat(name, '(할인상품)')
+       else name
+       end as name, price, amount, sale_amount, register_date from product;
