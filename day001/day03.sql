30f910c95fe092708d7ac355be57d7d39b9d01c4
minch13r <0414minyoung@naver.com>
2025-02-13 12:33:01 +0900

    -- 제품명 별 판매수량 / 판매합계 조회

--- day001/day03.sql
+++ day001/day03.sql
@@ -1,10 +1,4 @@
 use shop;
 
--- 칼럼 생성시 계산된 값을 자동계싼하여 생성하고 싶을 경우
--- ( generated columns ), columns는 생략 가능
-	-- stored : 값이 저장되는 방식
-	-- (데이터가 입력되거나, 수정될 때, 해당 칼럼도 같이 갱신)
-
-	-- virtual : 데이터를 저장하지 않고 정의만
-	-- (해당 칼럼을 읽으려고 시도할 때 계산을 통해 보여주는 것만)
-alter table buy add total int generated always as (price * amount) stored after amount;
+-- 제품명 별 판매수량 / 판매합계 조회
+select product_name, sum(amount), sum(price) from buy group by product_name;
