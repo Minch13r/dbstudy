cbb74dae622f21c8dc122a3228e45e3742d428a4
minch13r <0414minyoung@naver.com>
2025-02-13 12:34:06 +0900

    오늘 판매수량 / 판매합계 조회

--- day001/day03.sql
+++ day001/day03.sql
@@ -1,4 +1,4 @@
 use shop;
 
--- 제품명 별 판매수량 / 판매합계 조회
-select product_name, sum(amount), sum(price) from buy group by product_name;
+-- 오늘 판매수량 / 판매합계 조회
+select sum(amount) as 판매수량, sum(price) as 매출 from buy;
