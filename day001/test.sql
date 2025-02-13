895d0b2fa1b0fb95511d6a9d0667b3c2eb7e125b
minch13r <0414minyoung@naver.com>
2025-02-13 10:15:01 +0900

    전공과 학생수 정렬

--- day001/test.sql
+++ day001/test.sql
@@ -1,5 +1,3 @@
 use test;
 
--- limit 0,3 을 해줘야 범위지정 가능.
--- 3만 넣었을 때 무조건 처음부터 하기 때문에 범위지정하는 습관을 들이는게 좋음
-select name, score from student order by score desc limit 0,3;
+select major as 전공, count(major) as 학생수 from student group by major having count(major) < 3;
