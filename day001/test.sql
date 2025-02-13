89d2afbeff17269daab50625f202f366e695b107
minch13r <0414minyoung@naver.com>
2025-02-13 09:32:04 +0900

    데이터 삽입

--- day001/test.sql
+++ day001/test.sql
@@ -1,9 +1,7 @@
 use test;
 
-create table test2 (
-id int auto_increment,
-name varchar(10) not null,
-age int default 20,
-address varchar(20),
-primary key(id)
-);
+insert into test2 (name, age, address) values ('홍길동', 23, '서울');
+insert into test2 (name, age, address) values ('강길순', 24, '인천');
+insert into test2 (name, age, address) values ('이순신', 22, '서울');
+insert into test2 (name, age, address) values ('강감찬', 23, '인천');
+insert into test2 (name, age, address) values ('유관순', 21, '서울');
