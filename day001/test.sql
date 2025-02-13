445da8e7a21ed9991aa3a2a5569c1c6f36be0632
minch13r <0414minyoung@naver.com>
2025-02-13 09:35:07 +0900

    성별 성적 인원수, 평균 출력 => 평균이 큰 순으로 정렬(내림차순)

--- day001/test.sql
+++ day001/test.sql
@@ -1,3 +1,3 @@
 use test;
 
-select gender, avg(score) from student group by gender order by avg(score) desc;
+select gender as 성별, count(gender) as 인원, avg(score) as 평균 from student group by gender order by avg(score) desc;
