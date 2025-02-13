d8200ec899ad657e2669722080ef090ddb9db21f
minch13r <0414minyoung@naver.com>
2025-02-13 09:42:28 +0900

    범위지정 issue

--- day001/test.sql
+++ day001/test.sql
@@ -1,3 +1,5 @@
 use test;
 
-select major, count(major) as 학생수 from student group by major having count(major) < 3;
+-- limit 0,3 을 해줘야 범위지정 가능.
+-- 3만 넣었을 때 무조건 처음부터 하기 때문에 범위지정하는 습관을 들이는게 좋음
+select name, score from student order by score desc limit 0,3;
