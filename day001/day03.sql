ec3b19d06e8f911c17877acb25fe5a0b87a70e00
minch13r <0414minyoung@naver.com>
2025-02-13 17:32:49 +0900

    가장 많이 판매한 제품의 이름을 출력

--- day001/day03.sql
+++ day001/day03.sql
@@ -1,3 +1,3 @@
 use shop;
 
-select product_name, sum(amount) from buy group by product_name order by sum(amount) desc limit 1;
+select product_name, sum(amount) from buy group by product_name order by sum(amount) desc limit 0,1;
