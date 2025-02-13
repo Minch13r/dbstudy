580e32612567297aea3795faa374a2545e347573
minch13r <0414minyoung@naver.com>
2025-02-13 09:35:18 +0900

    각 과별 성적 합계가 250 이상인 학과만 출력

--- day001/test.sql
+++ day001/test.sql
@@ -1,3 +1,3 @@
 use test;
 
-select gender as 성별, count(gender) as 인원, avg(score) as 평균 from student group by gender order by avg(score) desc;
+select major, sum(score) from student group by major having sum(score) >= 250;
