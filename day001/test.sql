b08cf10e57ce6f69024a9a3fdde24055231601a6
minch13r <0414minyoung@naver.com>
2025-02-13 09:35:42 +0900

    20=1, 21=2, 22=3, 23이상은 4로 수정

--- day001/test.sql
+++ day001/test.sql
@@ -1,3 +1,6 @@
 use test;
 
-alter table student add grade int after age;
+update student set grade = 1 where age = 20;
+update student set grade = 2 where age = 21;
+update student set grade = 3 where age = 22;
+update student set grade = 4 where age >= 23;
