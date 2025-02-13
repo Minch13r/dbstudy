aeccde47be89a876b6fd2c485dd1a75651a50668
minch13r <0414minyoung@naver.com>
2025-02-13 14:21:37 +0900

    소수점 제외 할인가

--- day001/day03.sql
+++ day001/day03.sql
@@ -1,3 +1,3 @@
 use shop;
 
-select name as 상품명, register_date as 입고된날짜 from product where month(register_date)=11;
+select name as 할인상품, price as 정상가, round(price*0.9) as 할인가 from product where price >=16000;
