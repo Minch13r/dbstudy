8918894077407e89d44ca8e997fa571df73e0e31
minch13r <0414minyoung@naver.com>
2025-02-14 16:07:32 +0900

    A학점 학생들의 학번, 이름, 과목, (중간+기말+출석+과제) as 성적

--- day001/day04.sql
+++ day001/day04.sql
@@ -1,7 +1,7 @@
 use school;
 
--- F를 받은 학생 명단 (학번, 이름, 학과)
-select s.std_num, s.std_name, s.std_major from student s
+-- A학점 학생들의 학번, 이름, 과목, (중간+기말+출석+과제) as 성적
+select s.std_num, s.std_name, s.std_major, (a.at_mid + a.at_final + a.at_attend + a.at_hw) as 성적 from student s
 inner join attend a
 on s.std_num = a.at_std_num
-where a.at_score = 'F';
+where a.at_score = 'A';
