e65963d42b4edb688f3f7f14d804f1dd37836525
minch13r <0414minyoung@naver.com>
2025-02-14 09:36:35 +0900

    가장 많이 판매한 제품의 이름을 출력 => buy 테이블 기준 쿼리문을 2번 풀어서 진행

--- day001/day03.sql
+++ day001/day03.sql
@@ -1,3 +1,9 @@
 use shop;
 
-select product_name, sum(amount) from buy group by product_name order by sum(amount) desc limit 0,1;
+select product_name, sum(amount) as s from buy
+group by product_name
+having s = (
+select max(total_sum) from
+(select sum(amount) as total_sum from buy
+group by product_name) as p
+);
