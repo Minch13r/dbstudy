baf429be173c9e7692a4e60d19377925a4b15269
minch13r <0414minyoung@naver.com>
2025-02-14 16:20:52 +0900

    강철수가 수강하고 있는 과목명과 교수명 출력

--- day001/day04.sql
+++ day001/day04.sql
@@ -1,7 +1,9 @@
 use school;
 
--- 학점별 중간, 기말, 출석, 과제 평균 (null은 제외, 학점별 오름차순)
-select a.at_score, avg(a.at_mid) as 중간평균, avg(a.at_final) as 기말평균, avg(a.at_attend) as 출석평균, avg(a.at_hw) as 과제평균
-from attend a
-where a.at_score is not null
-group by a.at_score order by a.at_score asc;
+-- 강철수가 수강하고 있는 과목명과 교수명 출력
+select s.std_name, c.co_name, c.co_professor from student s
+inner join attend a
+on s.std_num = a.at_std_num
+inner join course c
+on a.at_co_code = c.co_code
+where s.std_name = '강철수';
