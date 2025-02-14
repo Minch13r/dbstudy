fdd07fc93163347b60915e5c05e2007e922e81a9
minch13r <0414minyoung@naver.com>
2025-02-14 12:39:24 +0900

    1학기의 점수만 채우기 at_mid(최대) : 40 / at_final(최대) : 40 / at_attend(최대) : 10 / at_hw(최대) : 10

--- day001/day04.sql
+++ day001/day04.sql
@@ -1,6 +1,12 @@
 use school;
 
-update attend set at_term = case
-    when at_num <= 10 then 1
-    else 2
-    end;
+update attend set at_mid=40, at_final=37, at_attend=9, at_hw=10 where at_num=1;
+update attend set at_mid=35, at_final=38, at_attend=8, at_hw=9 where at_num=2;
+update attend set at_mid=13, at_final=7, at_attend=1, at_hw=10 where at_num=3;
+update attend set at_mid=27, at_final=28, at_attend=5, at_hw=4 where at_num=4;
+update attend set at_mid=37, at_final=35, at_attend=7, at_hw=9 where at_num=5;
+update attend set at_mid=25, at_final=40, at_attend=8, at_hw=10 where at_num=6;
+update attend set at_mid=10, at_final=8, at_attend=4, at_hw=3 where at_num=7;
+update attend set at_mid=34, at_final=36, at_attend=9, at_hw=8 where at_num=8;
+update attend set at_mid=19, at_final=7, at_attend=5, at_hw=10 where at_num=9;
+update attend set at_mid=27, at_final=40, at_attend=10, at_hw=8 where at_num=10;
