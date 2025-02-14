8159271659cad19d26d7f2a391a502c9eea75dab
minch13r <0414minyoung@naver.com>
2025-02-14 15:48:11 +0900

    A학점을 받은 학생명단 (학번, 이름, 과목, 교수명)

--- day001/day04.sql
+++ day001/day04.sql
@@ -1,6 +1,9 @@
 use school;
 
-select s.std_num, s.std_name, a.at_score
-from attend a inner join student s
-on a.at_std_num = s.std_num
-where a.at_score = 'a';
+-- F를 받은 학생 명단 (학번, 이름, 학과)
+select s.std_num, s.std_name, s.std_major, c.co_professor from student s
+inner join attend a
+on s.std_num = a.at_std_num
+inner join course c
+on a.at_co_code = c.co_code
+where a.at_score = 'A';
