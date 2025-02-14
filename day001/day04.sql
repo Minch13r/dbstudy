23c63e38b764621e6f3865c6b03f0aa04fe8f448
minch13r <0414minyoung@naver.com>
2025-02-14 15:21:59 +0900

    학점이 A인 학생들의 명단 출력

--- day001/day04.sql
+++ day001/day04.sql
@@ -1,7 +1,6 @@
 use school;
 
-select s.std_num, s.std_name, a.at_score, a.at_repetition
-from attend a
-inner join student s
+select s.std_num, s.std_name, a.at_score
+from attend a inner join student s
 on a.at_std_num = s.std_num
-where a.at_repetition = 'y';
+where a.at_score = 'a';
