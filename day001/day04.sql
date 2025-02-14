e0979c992e4c95bccaa94771a6a7e9ace5fa3aaf
minch13r <0414minyoung@naver.com>
2025-02-14 15:48:21 +0900

    A학점을 받은 학생명단 (학번, 이름, 과목, 교수명)

--- day001/day04.sql
+++ day001/day04.sql
@@ -1,6 +1,6 @@
 use school;
 
--- F를 받은 학생 명단 (학번, 이름, 학과)
+-- A를 받은 학생 명단 (학번, 이름, 학과)
 select s.std_num, s.std_name, s.std_major, c.co_professor from student s
 inner join attend a
 on s.std_num = a.at_std_num
