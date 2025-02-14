fad5a5f3148727053afbdacf23cb635cea0dd572
minch13r <0414minyoung@naver.com>
2025-02-14 16:10:57 +0900

    과목별 (co_name) 별 중간, 기말 출석 과제 합계

--- day001/day04.sql
+++ day001/day04.sql
@@ -1,7 +1,8 @@
 use school;
 
--- A학점 학생들의 학번, 이름, 과목, (중간+기말+출석+과제) as 성적
-select s.std_num, s.std_name, s.std_major, (a.at_mid + a.at_final + a.at_attend + a.at_hw) as 성적 from student s
+-- 과목별 (co_name) 별 중간, 기말 출석 과제 합계
+select c.co_name, sum(a.at_mid) as 중간, sum(a.at_final) as 기말, sum(a.at_attend) as 출석, sum(a.at_hw) as 과제
+from course c
 inner join attend a
-on s.std_num = a.at_std_num
-where a.at_score = 'A';
+on c.co_code = a.at_co_code
+group by c.co_name;
