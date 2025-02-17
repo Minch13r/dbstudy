ff120177e056635f18b5f67ec8a05a0ff0515974
minch13r <0414minyoung@naver.com>
2025-02-17 17:47:03 +0900

    총계수 확인하지

--- day001/day03.sql
+++ day001/day03.sql
@@ -1,9 +1,5 @@
 use shop;
 
-select product_name, sum(amount) as s from buy
-group by product_name
-having s = (
-select max(total_sum) from
-(select sum(amount) as total_sum from buy
-group by product_name) as p
-);
+select product_name, sum(amount) from buy group by product_name
+having sum(amount) >= all (select sum(amount) from buy group by product_name);
+
